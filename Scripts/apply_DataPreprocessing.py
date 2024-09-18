ee07117ea2032aad6be2aa72ca43ca8f86a6e11d
Shikah Alsunaidi <52110709+shikahJS@users.noreply.github.com>
2024-09-18 09:58:37 +0300

    New tasks added

--- Scripts/apply_DataPreprocessing.py
+++ Scripts/apply_DataPreprocessing.py
@@ -8,7 +8,7 @@ import sys
 sys.set_int_max_str_digits(0)
 
 #Total number of preprocessing tasks currently offered
-PreprocessingTasksNo = 9
+PreprocessingTasksNo = 13
 
 def apply_DataPreprocessing(datasetName,dataDirPath='Default_InitialCombinedDataDir',PreprocessingTasks=['all']):
     try:
@@ -38,80 +38,101 @@ def apply_DataPreprocessing(datasetName,dataDirPath='Default_InitialCombinedData
 #-----------------------------------------------
 def call_PreprocessingTask(dataset,taskID,config_File):
     match taskID:
-        case '1' | 'DropDuplicateRows':
-            return PreprocessingTask1_DropDuplicateRows(dataset)
-        case '2' | 'FillMissingDataWithZero':
+        case '1' | 'ProcessBytecodes':
+            return PreprocessingTask1_ProcessBytecodes(dataset)
+        case '2' | 'ProcessABI':
+            return PreprocessingTask2_ProcessABI(dataset)
+        case '3' | 'ProcessConstructorArguments':
+            return PreprocessingTask3_ProcessConstructorArguments(dataset)
+        case '4' | 'ProcessOpcodes':
+            return PreprocessingTask4_ProcessOpcodes(dataset)
+        case '5' | 'DropDuplicateRows':
+            return PreprocessingTask5_DropDuplicateRows(dataset)
+        case '6' | 'FillMissingDataWithZero':
             dataToBeProcessed = get_Path('NullColsToZero',config_File)
-            return PreprocessingTask2_FillMissingDataWithZero(dataset,dataToBeProcessed)
-        case '3' | 'FillMissingDataWithNegativeOne':
+            return PreprocessingTask6_FillMissingDataWithZero(dataset,dataToBeProcessed)
+        case '7' | 'FillMissingDataWithNegativeOne':
             dataToBeProcessed = get_Path('NullColsToNegativeOne',config_File)
-            return PreprocessingTask3_FillMissingDataWithNegativeOne(dataset,dataToBeProcessed)
-        case '4' | 'RemoveUselesCols':
+            return PreprocessingTask7_FillMissingDataWithNegativeOne(dataset,dataToBeProcessed)
+        case '8' | 'RemoveUselesCols':
             dataToBeProcessed = get_Path('UselesCols',config_File)
-            return PreprocessingTask4_RemoveUselesCols(dataset,dataToBeProcessed)
-        case '5' | 'HandlingHexaData':
+            return PreprocessingTask8_RemoveUselesCols(dataset,dataToBeProcessed)
+        case '9' | 'HandlingHexaData':
             dataToBeProcessed = get_Path('HexaColsToInt',config_File)
-            return PreprocessingTask5_HandlingHexaData(dataset,dataToBeProcessed)
-        case '6' | 'HandlingStringNumericalData':
+            return PreprocessingTask9_HandlingHexaData(dataset,dataToBeProcessed)
+        case '10' | 'HandlingStringNumericalData':
             dataToBeProcessed = get_Path('StringNumColsToInt',config_File)
-            return PreprocessingTask6_HandlingStringNumericalData(dataset,dataToBeProcessed)
-        case '7' | 'HandlingStringBoolColsToInt':
+            return PreprocessingTask10_HandlingStringNumericalData(dataset,dataToBeProcessed)
+        case '11' | 'HandlingStringBoolColsToInt':
             dataToBeProcessed = get_Path('StringBoolColsToInt',config_File)
-            return PreprocessingTask7_HandlingStringBoolColsToInt(dataset,dataToBeProcessed)
-        case '8' | 'HandlingCategoricalData':
+            return PreprocessingTask11_HandlingStringBoolColsToInt(dataset,dataToBeProcessed)
+        case '12' | 'HandlingCategoricalData':
             dataToBeProcessed = get_Path('CategoricalCols',config_File)
-            return PreprocessingTask8_HandlingCategoricalData(dataset,dataToBeProcessed)
-        case '9' | 'SetDataIndexColumn':
+            return PreprocessingTask12_HandlingCategoricalData(dataset,dataToBeProcessed)
+        case '13' | 'SetDataIndexColumn':
             dataToBeProcessed = get_Path('IndexCol',config_File)
-            PreprocessingTask9_SetDataIndexColumn(dataset,dataToBeProcessed)
+            PreprocessingTask13_SetDataIndexColumn(dataset,dataToBeProcessed)
         # default pattern
         case _:
             print(taskID + ' is an incorrect Task ID')
 
 #Perform the chosen preprocessed task
 #------------------------------------
-def PreprocessingTask1_DropDuplicateRows(dataset):
+def PreprocessingTask1_ProcessBytecodes(dataset):
+    
+    return dataset
+def PreprocessingTask2_ProcessABI(dataset):
+
+    return dataset
+def PreprocessingTask3_ProcessConstructorArguments(dataset):
+
+    return dataset
+def PreprocessingTask4_ProcessOpcodes(dataset):
+    #Convert Opcodes values into a list
+    dataset['Opcodes'] = dataset['Opcodes'].str.split('<br>')
+    return dataset
+def PreprocessingTask5_DropDuplicateRows(dataset):
     dataset.drop_duplicates(keep='first',inplace=True)
     dataset.reset_index(drop=True,inplace=True)
     return dataset
-def PreprocessingTask2_FillMissingDataWithZero(dataset,NullColsToZero):
+def PreprocessingTask6_FillMissingDataWithZero(dataset,NullColsToZero):
     cols = list(set(dataset.columns) & set(NullColsToZero))
     dataset[cols] = dataset[cols].fillna(0)
     return dataset
-def PreprocessingTask3_FillMissingDataWithNegativeOne(dataset,NullColsToNegativeOne):
+def PreprocessingTask7_FillMissingDataWithNegativeOne(dataset,NullColsToNegativeOne):
     ## txreceipt_status values are 0 and 1, indicating failure and success, respectively.
     ## Blank means it is still waiting for confirmation
     cols = list(set(dataset.columns) & set(NullColsToNegativeOne))
     dataset[cols] = dataset[cols].fillna(-1)
     return dataset
-def PreprocessingTask4_RemoveUselesCols(dataset,UselesCols):
+def PreprocessingTask8_RemoveUselesCols(dataset,UselesCols):
     cols = list(set(dataset.columns) & set(UselesCols))
     dataset.drop(columns=cols,axis=1,inplace=True)
     return dataset
-def PreprocessingTask5_HandlingHexaData(dataset,HexaColsToInt):
+def PreprocessingTask9_HandlingHexaData(dataset,HexaColsToInt):
     cols = list(set(dataset.columns) & set(HexaColsToInt))
     ## convert hexa columns into integer
     for col in cols:
         dataset[col]=dataset[col].astype(str).apply(int, base=16)
     return dataset
-def PreprocessingTask6_HandlingStringNumericalData(dataset,StringNumColsToInt):
+def PreprocessingTask10_HandlingStringNumericalData(dataset,StringNumColsToInt):
     cols = list(set(dataset.columns) & set(StringNumColsToInt))
     dataset[cols]=dataset[cols].apply(pd.to_numeric)
     return dataset
-def PreprocessingTask7_HandlingStringBoolColsToInt(dataset,StringBoolColsToInt):
+def PreprocessingTask11_HandlingStringBoolColsToInt(dataset,StringBoolColsToInt):
     dataset = dataset.replace({'Yes':1,'No':0})
     '''cols = list(set(dataset.columns) & set(StringBoolColsToInt))  ## ToBeRemoved if "StringBoolColsToInt":["Experimental Features"] handled with the previous code line. Update config file and the call statement
     for col in cols:
         dataset[col] = np.where(dataset[col] != 0, 1, 0)'''
     return dataset
-def PreprocessingTask8_HandlingCategoricalData(dataset,CategoricalCols):
+def PreprocessingTask12_HandlingCategoricalData(dataset,CategoricalCols):
     cols = list(set(dataset.columns) & set(CategoricalCols)) ##  a special handling function might be added for Opcodes
     for col in cols:
         le= LabelEncoder()
         le.fit(dataset[col].unique().tolist())
         dataset[col]= le.transform(dataset[col])
     return dataset
-def PreprocessingTask9_SetDataIndexColumn(dataset,IndexCol):
+def PreprocessingTask13_SetDataIndexColumn(dataset,IndexCol):
     dataset.set_index(IndexCol,drop=True,inplace=True)
     return dataset
 #Get data to be preprocessed
